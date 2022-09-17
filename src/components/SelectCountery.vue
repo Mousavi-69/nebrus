9ab72aca223aec97642028ecb16d11e05d85e170
Mohamad <m.sanjarmoosavi@gmail.com>
2022-09-17 20:37:30 +0430

    The SelectCountery Component design is done

---
+++ src/components/SelectCountery.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+    const props = defineProps<{
+        country:string;
+        src_img:string;
+    }>()
+    console.log(props.src_img)
+    </script>
+<template>
+  <div class="m-5">
+    <div
+      class="w-full bg-white h-24 flex justify-between items-center rounded-3xl shadow-inherit drop-shadow-xl "
+    >
+      <img  src = '@/assets/aragh.jpg' class="w-28 h-full rounded-l-2xl" />
+      <h3 class="pr-5 text-2xl font-bold text-black  ">لهجه {{props.country}}</h3>
+    </div>
+  </div>
+</template>
